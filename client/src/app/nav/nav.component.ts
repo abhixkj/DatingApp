ee8062079e922cab50ea37df3f00cad2363070fe
Abhishek Kr Jha <abhishekk.jha@eagleview.com>
2022-01-16 15:58:52 +0530

    Adding User to local storage

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor() {}
+  loggedIn: boolean;
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
 
-  login()
-  {
-    console.log(this.model);
-    
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(
+      (user) => {
+        this.loggedIn = !!user;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 }
